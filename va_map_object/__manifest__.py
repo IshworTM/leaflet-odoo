c6678b055876e5f68ac27541fe4464efd4f0dfea
Ishwor <dulalishan3@gmail.com>
2025-02-24 11:35:05 +0545

    add init files
    Co-authored-by: GuitarTM <dulalishan13@gmail.com>

---
+++ va_map_object/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "VA MAP OBJECT",
+    "version": "1.0",
+    "summary": "A module that handles the map objects",
+    "description": "A module for objects.",
+    "author": "Vitruvian Analytica",
+    "license": "LGPL-3",
+    "depends": ["base", "va_map"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/map_objects_views.xml",
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
